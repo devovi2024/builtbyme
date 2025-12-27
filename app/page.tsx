2cd19c76fcedb29dce56ca65a5eb2a403fa82619
devovi2024 <arfanhosenovi@gmail.com>
2025-12-28 02:07:33 +0600

    nextjs application by built byme and add new features shascn and more...

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    
-
+    <div>
+      Hello Next.js
+      <Button variant="link" size="lg">Subscribe and Updates</Button>
+    </div>
   );
 }
---
+++ app/products/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Products() {
+  return (
+    <div>
+      This is Products Page
+    </div>
+  );
+}
